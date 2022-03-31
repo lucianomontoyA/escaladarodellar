5ee31ec4b3f15dfdcc03f5929a4ab59f4ad17179
lucianomontoyA <yoluchomas@hotmail.com>
2022-03-31 12:56:25 -0300

    js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ console.log("empieza")
 let nombre = prompt("Hola ,¿como te llamas?")
 console.log(nombre)
 alert("bienvenid@ " + nombre  + "  la aventura espera 🧗 ⛰️ ")
-
+ 
+for(let i = 1; i <=4; i ++) {
+    console.log("iteracion  ${i}");
+    }
+console.log("fin de ciclo")
